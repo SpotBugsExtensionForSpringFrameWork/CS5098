d127ec98092adc64cfc04d0eff248bfb29eb5794
yoochul <yk41@st-andrews.ac.uk>
2021-07-15 13:10:23 +0100

    Added sample empty Detector (still in progress)

---
+++ src/test/java/com/cs5098sbext/test/badpractice/ChangeableEnumsPersistenceDetectorTest.java
@@ -0,0 +1,26 @@
+package com.cs5098sbext.test.badpractice;
+
+import com.h3xstream.findbugs.test.BaseDetectorTest;
+import com.h3xstream.findbugs.test.EasyBugReporter;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class ChangeableEnumsPersistenceDetectorTest extends BaseDetectorTest {
+//    @Test
+//    public void testMethod() throws Exception {
+//        String[] files = {
+//                getClassFilePath("testcode/ChangeableEnumPersistenceDefectCode")
+//        };
+//        EasyBugReporter reporter = spy(new SecurityReporter());
+//        analyze(files, reporter);
+//
+//        verify(reporter).doReportBug(
+//                bugDefinition()
+//                        .bugType("EXTERNAL_CONFIG_CONTROL")
+//                        .inClass("DbCatalog").atLine(11)
+//                        .build()
+//        );
+//        verify(reporter, times(1)).doReportBug(bugDefinition().bugType("EXTERNAL_CONFIG_CONTROL").build());
+//    }
+}
